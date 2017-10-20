d80cf95589fb013be8b0ad16a7f7e02d60385106
upadhyayparam <Param.Upadhyay@dal.ca>
2017-10-20 19:17:53 +0000

    it should just seperate i and mac and give them into a list

---
+++ task04.py
@@ -0,0 +1,23 @@
+ import create-ticket.py
+
+
+
+fil= open(" file.py ", "r')
+line = file.strip()
+word = file.split()
+
+ip = []
+mac = []
+
+for i in range (len(word)):
+	if word[i] == "ip":
+	ip.append(word[i+2])
+for i in range (len(word)):
+	if word[i] == "mac"
+	mac.append(word[i+2])
+for in in range (len(word)):
+	print(mac[i], "==", ip[i])
+
+
+
+getnetworkdevicecount
